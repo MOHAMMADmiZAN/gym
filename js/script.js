efe0ac1961277dcdc2c6b4bff4a64e82c1c116dd
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-08-08 16:31:14 +0600

    gym

--- js/script.js
+++ js/script.js
@@ -84,3 +84,51 @@ $('.test-slider').slick({
         // instead of a settings object
     ]
 });
+
+
+///counter-up part///////
+$('.up').counterUp({
+    delay: 10,
+    time: 1000
+});
+/////sponsor-tab/////
+$('.sponsor-tab').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    centerPadding: '0',
+    prevArrow: '<i class="fas fa-angle-double-left prev"></i>',
+    nextArrow: '<i class="fas fa-angle-double-right next"></i>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
